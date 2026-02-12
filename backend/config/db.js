0f4c5f3e5fa23dd038ca262c595a0cf94c2bc434
Harshit <harshkush15sep@gmail.com>
2026-02-12 10:57:56 +0530

    server Database connection file seperated to secure.

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const DB_URI = process.env.MONGO_URI;
+    
+    if (!DB_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    const db = await mongoose.connect(DB_URI);
+
+    console.log(`Database Connected: successfully... ${db.connection.host}`);
+  } catch (error) {
+    console.error(`Data Base Connection Failed : ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,38 @@
-import mongoose from "mongoose";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
-import { app } from "./src/app.js";
+import connectDB from "./config/db.js";
+import userRouter from "./routes/user.routes.js";
+import batchRouter from './routes/batch.routes.js';
+import studentRouter from './routes/student.routes.js';
+import feeRouter from './routes/fee.routes.js';
+import homeRouter from "./routes/home.routes.js";
 
-dotenv.config({
-  path: './.env'
+
+dotenv.config({ path: './.env' });
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+connectDB();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use('/', homeRouter);
+app.use("/user", userRouter);
+app.use("/batch", batchRouter);
+app.use('/student', studentRouter);
+app.use('/fee', feeRouter);
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}...`);
 });
-const port = process.env.PORT || 4000
-const DB = process.env.MONGO_URI
-
-mongoose.connect(DB)
-  .then(() => console.log(" Database Connected"))
-  .catch(err => console.log(" DB Error:", err));
-
-
-app.listen(port , () => {
-  console.log(`Server running on port ${port}...`);
-});
-
-
