b51468bc9e1e6e9373ac98c10e9396c5f36049c2
Harshit <harshkush15sep@gmail.com>
2026-02-24 09:25:17 +0530

    refactor: update Dockerfile paths and improve database connection handling

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const DB_URI = process.env.MONGO_URI;
+    // const DB_URI = process.env.MONGO_URI;
     
-    if (!DB_URI) {
-      throw new Error("MONGO_URI is not defined in environment variables");
-    }
-
-    const db = await mongoose.connect(DB_URI);
+    // if (!DB_URI) {
+    //   throw new Error("MONGO_URI is not defined in environment variables");
+    // }
+    
+    const db = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`Database Connected: successfully... ${db.connection.host}`);
   } catch (error) {
