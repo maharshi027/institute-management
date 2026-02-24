e8087ce2a88fc863f553d2d7cb4cb0fd875bd497
Harshit <harshkush15sep@gmail.com>
2026-02-24 09:42:24 +0530

    cors security changing

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ connectDB();
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
-  credentials: true
+  
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
