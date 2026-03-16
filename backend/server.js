e8cb50480e2faa7d6f4dd894e3a55afa8892ddda
Harshit <harshkush15sep@gmail.com>
2026-03-16 19:42:04 +0530

    policy changed for demo

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,11 @@ const port = process.env.PORT || 5000;
 // app.use(
 //   cors({
 //     origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
-//     credentials: true,
 //     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
 //   })
 // );
+
 app.use(cors());
 
 app.use(express.json());
