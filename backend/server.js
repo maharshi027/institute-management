15ab710d9881d3745305a487f77dee4f52f3fa43
Harshit <harshkush15sep@gmail.com>
2026-03-16 19:34:42 +0530

    cors installation

--- backend/server.js
+++ backend/server.js
@@ -17,15 +17,15 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
+// app.use(
+//   cors({
+//     origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
